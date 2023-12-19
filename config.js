1104367005b4d4583163b7d03bf4bf7bee80c5db
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-19 08:49:18 +0100

    new

--- config.js
+++ config.js
@@ -320,6 +320,26 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
         },
+
+        {
+            id: 'third-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Delta State',
+            image: 'src/images/Ogwashi-1.png', 
+            description: 'Construction of Road at Ogwashi Uku, Delta State',
+            location: {
+                center: [6.71742, 6.19109],
+                zoom: 10.78,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
         {
             id: 'second-identifier',
             alignment: 'right',
